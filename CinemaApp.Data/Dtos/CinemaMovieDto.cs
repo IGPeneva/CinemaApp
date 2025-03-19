20bd4e63fc22bfaa9d866091d3edbdfa72941f9c
IGPeneva <engr_peneva@abv.bg>
2025-03-19 22:14:39 +0200

    Refactor DataProcessor

--- CinemaApp.Data/Dtos/CinemaMovieDto.cs
+++ CinemaApp.Data/Dtos/CinemaMovieDto.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CinemaApp.Common.Constants;
+using Newtonsoft.Json;
+
 
 namespace CinemaApp.Data.Dtos
 {
     internal class CinemaMovieDto
     {
-        public string Movie { get; set; }
-        public string Cinema { get; set; }
+        [Required]
+        [MinLength(EntityConstants.Movie.TitleMinLength)]
+        [MaxLength(EntityConstants.Movie.TitleMaxLength)]
+        public string Movie { get; set; } = null!;
+
+        [Required]
+        [MinLength(EntityConstants.Cinema.NameMinLength)]
+        [MaxLength(EntityConstants.Cinema.NameMaxLength)]
+        public string Cinema { get; set; } = null!;
+
+        [Required]
+        [Range(EntityConstants.CinemaMovie.AvailableTicketsMinValue,
+            EntityConstants.CinemaMovie.AvailableTicketsMaxValue)]
         public int AvailableTickets { get; set; }
+
+        [Required]
         public bool IsDeleted { get; set; }
-        public string Showtimes { get; set; }
+        
+        public string? Showtimes { get; set; }
     }
 }
--- CinemaApp/Program.cs
+++ CinemaApp/Program.cs
@@ -1,8 +1,10 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using CinemaApp.Data;
 using CinemaApp.Data.Models;
 using CinemaApp.Data.Utilities;
+using CinemaApp.Data.Utilities.Interfaces;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,13 +12,27 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("CinemaDbConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<CinemaDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IValidator, EntityValidator>();
+builder.Services.AddSingleton<IXmlHelper, XmlHelper>();
+builder.Services.AddScoped<IDbSeeder, DataProcessor>();
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options => options.SignIn.RequireConfirmedAccount = false)
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+
+        options.Password.RequireDigit = true;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 3;
+    })
     .AddRoles<IdentityRole<Guid>>()
     .AddEntityFrameworkStores<CinemaDbContext>();
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -25,7 +41,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
-    //app.UseDeveloperExceptionPage();
 }
 else
 {
@@ -39,24 +54,21 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication();//Who am I? -> Login with username and password
-app.UseAuthorization();//What I am allowed to do? -> I know who you are,I tell you what you can do
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
-using (var scope = app.Services.CreateScope())
+if (app.Environment.IsDevelopment())
 {
-    var services = scope.ServiceProvider;
-    var dbContext = services.GetRequiredService<CinemaDbContext>();
+    using IServiceScope scope = app.Services.CreateScope();
+    IServiceProvider services = scope.ServiceProvider;
 
-    await DataProcessor.ImportMoviesFromJson(dbContext);
-    await DataProcessor.ImportCinemasMoviesFromJson(dbContext);
-    //await DataProcessor.ImportTicketsFromXml(dbContext);
-
+    IDbSeeder dataProcessor = services.GetRequiredService<IDbSeeder>();
+    await dataProcessor.SeedData();
 }
-
 
 app.Run();
